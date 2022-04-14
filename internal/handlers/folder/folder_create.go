0ed8d05e9bcf49774dcd203abe54c1d0d08fea07
Noah <noah231515@gmail.com>
2022-04-14 08:54:05 -0400

    create folder

--- internal/handlers/folder/folder_create.go
+++ internal/handlers/folder/folder_create.go
@@ -7,8 +7,12 @@ import (
 	"vaultea/api/internal/database"
 	"vaultea/api/internal/handlers"
 	"vaultea/api/internal/models"
+	crypto_utils "vaultea/api/internal/utils/crypto"
 	http_utils "vaultea/api/internal/utils/http"
 	"vaultea/api/internal/validators"
+
+	jwtmiddleware "github.com/auth0/go-jwt-middleware/v2"
+	"github.com/auth0/go-jwt-middleware/v2/validator"
 )
 
 type CreateProcedure struct {
@@ -29,10 +33,30 @@ func (CreateProcedure) ValidateData(proc *handlers.ProcedureData) bool {
 
 func (CreateProcedure) Execute(proc *handlers.ProcedureData) {
 	db := database.GetDb()
+	context := proc.Request.Context()
+	claims := context.Value(jwtmiddleware.ContextKey{}).(*validator.ValidatedClaims).CustomClaims.(*crypto_utils.Claims)
+	user := models.User{}
+	vault := models.Vault{}
+
+	result := db.Where("username = ?", claims.Username).First(&user)
+	if result.Error != nil {
+
+		proc.Writer.WriteHeader(500)
+		proc.Writer.Write([]byte("Error validating JWT."))
+		return
+	}
+
+	vaultResult := db.Where("user_id = ?", user.ID).First(&vault)
+	if vaultResult.Error != nil {
+		proc.Writer.WriteHeader(500)
+		proc.Writer.Write([]byte("Error validating JWT."))
+		return
+	}
 
 	folder := models.Folder{
 		Name:        proc.BodyMap["name"].(string),
 		Description: proc.BodyMap["description"].(string),
+		VaultID:     vault.ID,
 	}
 
 	if proc.BodyMap["folderId"] != nil {
