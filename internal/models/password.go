af574d2e333e30af6d6ab15576fde307b2c6817b
Noah <noah231515@gmail.com>
2022-04-28 08:53:59 -0400

    allow folder id to null out

--- internal/models/password.go
+++ internal/models/password.go
@@ -6,10 +6,10 @@ import (
 
 type Password struct {
 	BaseModel
-	VaultID     uint  `gorm:"not null" json:"-"`
-	Vault       Vault `json:"-"`
-	FolderID    uint  `json:"folderId"`
-	Folder      Folder
+	VaultID     uint      `gorm:"not null" json:"-"`
+	Vault       Vault     `json:"-"`
+	FolderID    *uint     `json:"folderId"`
+	Folder      *Folder   `json:"-"`
 	Name        string    `gorm:"not null" json:"name"`
 	Username    string    `gorm:"not null" json:"username"`
 	Password    string    `gorm:"not null" json:"password"`
