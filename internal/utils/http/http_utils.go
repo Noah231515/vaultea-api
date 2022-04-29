d1f62eb5acc97d7f2ebd2374f4f1ae2bbcf9e376
Noah <noah231515@gmail.com>
2022-04-29 09:00:23 -0400

    using vault id from claims

--- internal/utils/http/http_utils.go
+++ internal/utils/http/http_utils.go
@@ -5,26 +5,12 @@ import (
 	"io/ioutil"
 	"net/http"
 	"reflect"
-	"vaultea/api/internal/database"
 	"vaultea/api/internal/models"
 	crypto_utils "vaultea/api/internal/utils/crypto"
 
 	jwtmiddleware "github.com/auth0/go-jwt-middleware/v2"
 	"github.com/auth0/go-jwt-middleware/v2/validator"
 )
-
-func GetRequestBodyMap(request *http.Request) map[string]interface{} {
-	bodyMap := make(map[string]interface{})
-
-	b, err := ioutil.ReadAll(request.Body)
-
-	if err != nil {
-		panic(err)
-	}
-	json.Unmarshal(b, &bodyMap)
-
-	return bodyMap
-}
 
 func GetBodyData(request *http.Request, dataType reflect.Type) (interface{}, error) {
 	var jsonErr error
@@ -66,31 +52,8 @@ func WriteErrorResponse(writer http.ResponseWriter, err error) {
 	writer.Write([]byte(err.Error()))
 }
 
-func IsPost(request *http.Request) bool {
-	return request.Method == http.MethodPost
-}
-
-func GetVaultId(writer http.ResponseWriter, request *http.Request) uint { // TODO: Clean up
-	db := database.GetDb()
-	user := models.User{}
-	vault := models.Vault{}
+func GetVaultId(writer http.ResponseWriter, request *http.Request) uint {
 	context := request.Context()
-
 	claims := context.Value(jwtmiddleware.ContextKey{}).(*validator.ValidatedClaims).CustomClaims.(*crypto_utils.Claims)
-
-	result := db.Where("username = ?", claims.Username).First(&user)
-	if result.Error != nil {
-		writer.WriteHeader(500)
-		writer.Write([]byte("Error validating JWT."))
-		return 0
-	}
-
-	vaultResult := db.Where("user_id = ?", user.ID).First(&vault)
-	if vaultResult.Error != nil {
-		writer.WriteHeader(500)
-		writer.Write([]byte("Error validating JWT."))
-		return 0
-	}
-
-	return vault.ID
+	return claims.VaultID
 }
