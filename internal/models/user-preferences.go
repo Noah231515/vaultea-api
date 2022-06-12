abd66e8aaa41dcd9611c05507866dc8c4b1ebc3d
Noah <noah231515@gmail.com>
2022-06-12 17:19:40 -0400

    add toggle vault_view route

--- internal/models/user-preferences.go
+++ internal/models/user-preferences.go
@@ -4,5 +4,5 @@ type UserPreferences struct {
 	BaseModel
 	UserID    uint `json:"-"`
 	User      User `json:"-"`
-	VaultView int  `gorm:"not null; default: 0; check: vault_view <> 0 <> 1" json:"vaultView"`
+	VaultView int  `gorm:"not null; default: 0; check:vault_view = 0 or vault_view = 1" json:"vaultView"`
 }
